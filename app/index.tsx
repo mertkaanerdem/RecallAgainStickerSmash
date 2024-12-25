07bd50ca9b31844ef03be0bafa7e9e6d1e2a594c
mertkaanerdem <mertkaanerdem@gmail.com>
2024-12-25 20:42:58 +0100

    cleared the default app

---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}
+    >
+      <Text style={styles.text}>Hello Expo and Mertkaan</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#25292e",
+  },
+  text: {
+    color: "#ffffff",
+  },
+});
