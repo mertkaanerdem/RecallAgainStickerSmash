f8b297d6679d0c51cd3ad58129629b6c9ac0b52d
mertkaanerdem <mertkaanerdem@gmail.com>
2024-12-25 22:20:53 +0100

    created 404 page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,5 +4,6 @@ export default function RootLayout() {
   return <Stack>
          <Stack.Screen name="index" options={{ title: 'Home' }} />
          <Stack.Screen name="about" options={{ title: 'About' }} />
+         <Stack.Screen name="not-found" options={{headerLeft: () => <></>}} />
   </Stack>;
 }
