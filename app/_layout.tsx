2ccda067fbadc9af4ae59329dbe4a0026c450a1e
mertkaanerdem <mertkaanerdem@gmail.com>
2024-12-26 15:36:05 +0100

    StatusBar is ok

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,15 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+
 export default function RootLayout() {
-  return(
-  <>
-  <StatusBar style="light" />
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" options={{ headerLeft: () => <></> }} />
-    </Stack>
-  </>
+  return (
+    <>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="light" />
+    </>
+
   );
 }
